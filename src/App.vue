5988ae791a90d7be10361bca1884da77435fa12c
xiaohaikid <xiaohaikid@163.com>
2020-08-28 15:19:53 +0800

    'first'

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <router-view />
+    <main-tab-bar />
+  </div>
 </template>
 
 <script>
+import MainTabBar from "components/content/mainTabbar/MainTabBar";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    MainTabBar,
+  },
 };
 </script>
 
 <style>
-@import url("./assets/css/base.css");
+@import url("assets/css/base.css");
 </style>
---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+    <div :style="activeStyle">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: "red",
+    },
+  },
+  data() {
+    return {
+      // isActive: true,
+    };
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.path) !== -1;
+    },
+    activeStyle() {
+      return this.isActive ? { color: this.activeColor } : {};
+    },
+  },
+  methods: {
+    itemClick() {
+      // console.log("itemClick");
+      this.$router.replace(this.path).catch((err) => err);
+    },
+  },
+};
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 12px;
+}
+.tab-bar-item img {
+  width: 30px;
+  height: 30px;
+  margin-top: 3px;
+  /* 去除图片下面默认的3像素 */
+  vertical-align: middle;
+  margin-top: 2px;
+}
+</style>
