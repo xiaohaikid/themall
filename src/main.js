74ba434f456993fa5f5de4b1dec591ec4ae6ce75
xiaohaikid <xiaohaikid@163.com>
2020-09-03 01:28:34 +0800

    '购物车'

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 
 import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
 
@@ -11,6 +12,9 @@ Vue.prototype.$bus = new Vue()
 
 // 解决移动端300ms的延迟
 FastClick.attach(document.body)
+
+// 使用懒加载的插件
+Vue.use(VueLazyLoad)
 
 new Vue({
   render: h => h(App),
